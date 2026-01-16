3b7359a1ea137c5076c4ddfbad09c4db528df1c5
Komol Parpixodjayev <www.komol8689@gmail.com>
2026-01-16 16:18:42 +0500

    add

---
+++ main_1.java
@@ -0,0 +1,14 @@
+public class main_1 {
+    public static void main(String[] args) {
+        // print: kursorni shu yerda qoldiradi
+        System.out.print("Olma ");
+        System.out.print("Anor ");
+
+        // println: chiqaradi va yangi qatorga o'tadi
+        System.out.println("Mevalar");
+
+        // printf: sonni chiroyli chiqaradi (masalan, 2 ta xona aniqlikda)
+        double narx = 15.556;
+        System.out.printf("Narxi: %.2f so'm",narx);
+    }
+}
---
+++ main_2.java
@@ -0,0 +1,10 @@
+public class main_2  {
+    public static void main(String[] args){
+        System.out.println("Komol");
+        int son=12345;
+        double kasr=19.999;
+        char harf = 'J';
+        boolean check=true;
+        String matn="Komol qalesan";
+        System.out.printf("Son: %d, Kasr: %.2f, Harf: %c, Boolean: %b, Matn: %s%n",son, kasr, harf, check, matn);    }
+}
