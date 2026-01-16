9c528c7fdf481568f44bb0a322360e1bbd28c5a0
Komol Parpixodjayev <www.komol8689@gmail.com>
2026-01-16 18:10:34 +0500

    add

--- main_2.java
+++ main_2.java
@@ -2,9 +2,23 @@ public class main_2  {
     public static void main(String[] args){
         System.out.println("Komol");
         int son=12345;
-        double kasr=19.999;
+        double kasr=1999999.999;
+        float kasr2= 229.994f;
         char harf = 'J';
         boolean check=true;
         String matn="Komol qalesan";
-        System.out.printf("Son: %d, Kasr: %.2f, Harf: %c, Boolean: %b, Matn: %s%n",son, kasr, harf, check, matn);    }
+//        System.out.printf(
+//                "Son: %d," +
+//                " Kasr: %.2f, " +
+//                "Kasr2: %.2f "+
+//                "Harf: %c, " +
+//                "Boolean: %b, " +
+//                "Matn: %s%n",
+//                son, kasr,kasr2, harf, check, matn);
+//        System.out.printf("Salom: %.2e %n",kasr2);
+//        System.out.printf("%S ism\n",matn);
+        System.out.printf("%,f \n",kasr);
+        System.out.printf("%d",son);
+
+    }
 }
---
+++ main_4.java
@@ -0,0 +1,8 @@
+public class main_4 {
+    public  static void main(String[] args){
+        int[] sonlar = {14, 7, 19, 3, 6, 10};
+        for(int i=0;i<sonlar.length;i++){
+            System.out.printf("%d \n",sonlar[i]);
+        }
+    }
+}
