b1b2a788262094099e68e67ba48e327a43fd5515
Komol Parpixodjayev <www.komol8689@gmail.com>
2026-01-20 19:01:12 +0500

    add

---
+++ OOP/day4/Person.java
@@ -0,0 +1,41 @@
+package OOP.day4;
+
+public abstract class Person {
+
+    private String name;
+    private int age;
+
+    public void show() { System.out.println(name + ", " + age); }
+
+    public abstract void workInfo();
+    // getter setter metodlar.
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public int getAge() {return age;}
+    public void setAge(int age) {this.age = age;}
+}
+
+// Driver class
+ class Driver extends Person {
+    @Override
+    public void workInfo() {
+        System.out.println("I am a driver");
+    }
+}
+
+// Teacher class
+ class Teacher extends Person {
+    @Override
+    public void workInfo() {
+        System.out.println("I am teaching");
+    }
+}
+
+
+// Cook class (oshpaz)
+ class Cook extends Person {
+    @Override
+    public void workInfo() {
+        System.out.println("I am cooking");
+    }
+}
