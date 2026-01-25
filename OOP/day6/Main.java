8c955f54205a348b7e87185a003ed22d527b7ef1
Komol Parpixodjayev <www.komol8689@gmail.com>
2026-01-25 19:25:45 +0500

    add

---
+++ OOP/day6/Main.java
@@ -0,0 +1,19 @@
+package OOP.day6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ismingizni kiriting: ");
+        String name = scanner.next();
+
+        System.out.print("Yoshingizni kiriting: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Kasbingiz: ");
+        String job = scanner.next();
+        System.out.println("Salom " + name + ". Sizning yoshingiz: " + age + " da." + "Siz " + job + " siz.");
+    }
+}
